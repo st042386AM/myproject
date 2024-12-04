e3510493f8b92309cc8e2eac2372f54edd38e2b4
Akihiro Matsuya <st042386@m02.kyoto-kcg.ac.jp>
2024-12-04 15:55:17 +0900

    thank_you.phpの追加

---
+++ thank_you.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>送信完了</title>
+</head>
+<body>
+    <h1>お問い合わせありがとうございました</h1>
+    <p>ご入力いただいた内容は正常に送信されました。</p>
+    <a href="index.php">トップページに戻る</a>
+</body>
+</html>
