1c01ac4d2a928bd7e99c76e2f767caac27d83dd1
Akihiro Matsuya <st042386@m02.kyoto-kcg.ac.jp>
2025-01-16 15:55:14 +0900

    新着情報のタグ付けに関する修正・追加

--- submit_news.php
+++ submit_news.php
@@ -3,10 +3,15 @@ require 'db.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $title = $_POST['title'] ?? '';
-    
+    $genre = $_POST['genre']; // タグ情報を取得
+
     if (!empty($title)) {
-        $stmt = $pdo->prepare("INSERT INTO news (title, created_at) VALUES (:title, NOW())");
-        $stmt->execute(['title' => $title]);
+        //$stmt = $pdo->prepare("INSERT INTO news (title, created_at) VALUES (:title, NOW())");
+        //$stmt->execute(['title' => $title]);
+        $stmt = $pdo->prepare('INSERT INTO news (title, genre, created_at) VALUES (:title, :genre, NOW())');
+        $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+        $stmt->bindParam(':genre', $genre, PDO::PARAM_STR);
+        $stmt->execute();
     }
 
     header('Location: index.php');  // 投稿後にトップページにリダイレクト
